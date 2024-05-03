bb57f7841f750e72e05ca74550ffd87e4e4c4aa9
TATIANA KORNEVA <100599584+kortana2302@users.noreply.github.com>
2024-05-03 21:43:05 +0300

    Update main2.py

--- main2.py
+++ main2.py
@@ -101,7 +101,7 @@ if st.button('Расчет') and len(df)>0:
     col_numeric.remove('pharmacyDistance')
     X_data = data.drop('price', axis = 1)
     col1.write(X_data.columns)
-    clo1.write(col_numeric)
+    col1.write(col_numeric)
     transformer = MinMaxScaler().fit(X_data[col_numeric])
     X_data[col_numeric] = transformer.transform(X_data[col_numeric])
 
